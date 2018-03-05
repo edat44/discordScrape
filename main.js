356c4078a534b1598b9e84aa0b1e0f516b826622
Edward Atkinson <eatkins1@nd.edu>
2018-03-04 22:32:03 -0500

    basic scraping works with post request to server

---
+++ main.js
@@ -0,0 +1,22 @@
+chrome.runtime.onMessage.addListener(function(request, sender, callback) {
+
+    $.ajax({
+        type: request.method,
+        url: request.url,
+        data: request.data,
+        contentType: "text/plain",
+        dataType: 'json',
+        success: function(responseText){
+            callback(responseText);
+        },
+        error: function(xhr, textStatus, errorThrown) {
+            //if required, do some error handling
+            callback('failed to connect to ' + request.url +
+            '\nerror: ' + errorThrown +
+            '\ntextStatus: ' + textStatus +
+            '\nxhr: ' + xhr.status);
+        }
+    });
+
+    return true; // prevents the callback from being called too early on return
+});
--- scrape.js
+++ scrape.js
@@ -2,14 +2,22 @@ function getUsers() {
     var guild = $(".name-3gtcmp:first").text()
     console.log("guild: " + guild)
     var members = []
-    $(".member").each(function(i) {
+    $(".member-username-inner").each(function(i) {
         members.push($(this).text());
     });
     console.log(members);
+    data = {"guild": guild, "members": members};
+    chrome.runtime.sendMessage({
+            method: 'POST',
+            url: 'http://dsg1.crc.nd.edu:5001/cse30246/discorddashboard/test',
+            data: JSON.stringify(data)
+        }, function(responseText) {
+            console.log(responseText);
+    });
 }
 
 $(document).ready(function() {
     setInterval(function() {
         getUsers();
-    }, 5000);
+    }, 3000);
 });
